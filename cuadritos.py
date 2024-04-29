84ceca7dcebd9c85b6cc679e9217dd6a94d3f020
lauraax <64974256+lauraax@users.noreply.github.com>
2024-04-29 07:59:44 -0500

    upload

--- cuadritos.py
+++ cuadritos.py
@@ -7,20 +7,20 @@ class Cuadritos(Sprite):
         Sprite.__init__(self)
         self.tablero = tablero
         self.cont = cont
+        self.tamaño = 128
         self.imagen = pygame.image.load("imagenes/cuadrito.png")
         self.rect = self.imagen.get_rect()
-        self.vel = 5
         self.rect.move_ip(cont[0]//4, cont[1]//5)
 
     def actualizar_posicion(self, keys):
         if keys[pygame.K_LEFT]:
-            self.rect.x -= self.vel
+            self.rect.x -= 1
         if keys[pygame.K_RIGHT]:
-            self.rect.x += self.vel
+            self.rect.x += 1
         if keys[pygame.K_UP]:
-            self.rect.y -= self.vel
+            self.rect.y -= 1
         if keys[pygame.K_DOWN]:
-            self.rect.y += self.vel
+            self.rect.y += 1
 
         # Restringir el rectángulo dentro de los límites del tablero
         self.rect.clamp_ip(self.tablero.rect)
--- juego_cuadrito.py
+++ juego_cuadrito.py
@@ -3,26 +3,44 @@ from pygame.sprite import Sprite
 from pygame.locals import *
 from tablero import *
 from cuadritos import *
+
+#definimos colores
+BLACK = (0,0,0)
+
 # definir el tamaño de la pantalla
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 reloj = pygame.time.Clock()
 
+#definimos variables a utilizar
+GAME_SIZE = 3
+
 #asignamos la clase Tablero al objeto tablero
 tablero = Tablero(size)
 cuadrito = Cuadritos(tablero, size)
+
+
 def main ():
     pygame.init
     pygame.mixer.init()
     
-
+    #fondo
     background_image = pygame.image.load("imagenes/space.jpg")
     background_rect = background_image.get_rect()
-
     pygame.display.set_caption("Puzzle")
+    surface = pygame.Surface((tablero.rect.width, tablero.rect.height))
+
+    #funcion dibujar cuadricula
+    def dibujar_cuadricula(surface, cuadrito):
+        for fila in range(-1, tablero.rect.height, cuadrito):
+            pygame.draw.line(surface, BLACK, (0, fila), (tablero.rect.width, fila), 2)
+        for columna in range(-1, tablero.rect.width, cuadrito):
+            pygame.draw.line(surface, BLACK, (columna, 0), (columna, tablero.rect.height), 2)
+
+
+    game = True
     
-    
-    while 1:
+    while game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -31,8 +49,12 @@ def main ():
         screen.blit(tablero.image, tablero.rect)
         screen.blit(cuadrito.imagen, cuadrito.rect)
         keys = pygame.key.get_pressed()
-
         cuadrito.actualizar_posicion(keys)
+
+        image_copy = tablero.image.copy()
+        dibujar_cuadricula(image_copy, cuadrito.tamaño)
+        screen.blit(image_copy, tablero.rect)
+        
 
         pygame.display.update()
         reloj.tick(60)
