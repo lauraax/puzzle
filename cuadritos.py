bb6773c95ab763367423892e4d07ea79760570d2
lauraax <laura.paez2405@gmail.com>
2024-05-06 02:34:22 -0500

    ya randomiza

--- cuadritos.py
+++ cuadritos.py
@@ -15,13 +15,17 @@ class Cuadritos(pygame.sprite.Sprite):
         super().__init__()
         self.number = number
         self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
-        self.image.fill(WHITE)
+        #self.image.fill(WHITE)
         pygame.draw.rect(self.image, COLORS[number % len(COLORS)], (3, 3, TILE_SIZE - 6, TILE_SIZE - 6))
-        pygame.draw.rect(self.image, BLACK, (0, 0, TILE_SIZE, TILE_SIZE), 3)
+        #pygame.draw.rect(self.image, BLACK, (0, 0, TILE_SIZE, TILE_SIZE), 3)
         font = pygame.font.Font(None, 36)
-        text = font.render(str(number), True, WHITE)
-        text_rect = text.get_rect(center=(TILE_SIZE // 2, TILE_SIZE // 2))
-        self.image.blit(text, text_rect)
+        self.text = font.render(str(self.number), True, WHITE)
+        self.text_rect = self.text.get_rect(center=(TILE_SIZE // 2, TILE_SIZE // 2))
+        self.image.blit(self.text, self.text_rect)
         self.rect = self.image.get_rect(topleft=(x * TILE_SIZE, y * TILE_SIZE))
         self.x = x
         self.y = y
+    
+    def __str__(self) -> str:
+        return str(self.number)
+
--- juego_cuadrito.py
+++ juego_cuadrito.py
@@ -11,45 +11,46 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Puzzle Deslizante")
         self.cuadritos = []
-        self.mostrar_inicio = True
-        #hacemos la matriz de cuadritos
-        for y in range(FILAS):
-            for x in range(COL):
-               if (x, y) != (COL - 1, FILAS -1):
-                   self.cuadritos.append(Cuadritos(y * COL + x + 1, x, y)) 
-        self.cuadro_vacio = CuadroVacio(COL - 1, FILAS - 1)
-        random.shuffle(self.cuadritos) #se supone q esto desordena los cuadritos pero no :c
+        
+        # hacemos la lista de cuadritos
+
+        coordenadas = [(x, y) for y in range(FILAS) for x in range(COL) if (x, y) != (COL - 1, FILAS - 1)]#creamos una lista de coordenadas para los cuadritos excluyendo la pos del vacio
+        random.shuffle(coordenadas)  #las randomizamos
+        for i in range(FILAS * COL - 1):  # hacemos el bucle sin que agregue un cuadro adicional
+            valor = i + 1 #numero del cuadrito
+            x, y = coordenadas[i]
+            cuadrito = Cuadritos(valor, x, y)
+            self.cuadritos.append(cuadrito)  #agregar el cuadrito a la lista de cuadritos
+        
+        random.shuffle(self.cuadritos) #se supone q esto desordena los cuadritos pero no :c 
+        self.cuadro_vacio = CuadroVacio(COL - 1, FILAS - 1) 
+ 
+        
+
     #funcion para hacer el blit de cada cuadrito en la lista cuadritos
     def dibujar_cuadritos(self):
         self.screen.fill(BLACK)
         for cuadrito in self.cuadritos:
             self.screen.blit(cuadrito.image, cuadrito.rect)
+        # for cuadrito in self.cuadritos:
+        #     print(cuadrito)
         pygame.display.flip()
+        
 
     #funcion para que el cuadro vacio se mueva y este mueva los otros cuadritos
     def mover_cuadrito(self, dx, dy):
         x, y = self.cuadro_vacio.x + dx, self.cuadro_vacio.y + dy #establecemos las cordenadas
+
         for cuadrito in self.cuadritos:
             if cuadrito.rect.topleft == (x * TILE_SIZE, y * TILE_SIZE):
                 cuadrito.rect.topleft = self.cuadro_vacio.x * TILE_SIZE, self.cuadro_vacio.y * TILE_SIZE
                 self.cuadro_vacio.x, self.cuadro_vacio.y = x, y
                 return
-            
-    def juego_terminado(self):
-        for i, cuadrito in enumerate(self.cuadritos): #recorremos la lista y si cada posicion coincide con la enumeracion el jugador gana
-            if cuadrito.number != i + 1:
-                return False
-            else:
-                return False
-            
-    def dibujar_texto(self):
-        if self.mostrar_inicio:
-            fuente = pygame.font.SysFont(None, 30)
-            texto = fuente.render("Ganaste", True, WHITE)
-            self.screen.blit(texto, ((WIDTH - texto.get_width()) // 10, HEIGHT - 50))
+
+
 
     def start(self):
-
+        
         running = True
         while running:
             for event in pygame.event.get():
@@ -64,14 +65,11 @@ class Game:
                         self.mover_cuadrito(0, 1)
                     elif event.key == pygame.K_DOWN:
                         self.mover_cuadrito(0, -1)
-                    elif event.key == pygame.K_SPACE:
-                        self.barajar_cuadritos()
-
+                    
+            
             self.dibujar_cuadritos()
+            
 
-            if self.juego_terminado():
-                running = False
-                
 
         pygame.quit()
         sys.exit()
